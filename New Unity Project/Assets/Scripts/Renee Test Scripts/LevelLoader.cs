63996327e86536da956aa499c9ccd3ea7f1d2c07
x3n0vial <69580639+x3n0vial@users.noreply.github.com>
2022-02-15 08:44:19 +0800

    Added scenes + level loader + some scripts + fadein animation + temp art asset to test
    Main scene
    Test scene
    Dsetroy trail script
    Dialogue script
    Dialogue manager script
    dialogue trigger script
    mouse cursor script
    level loader script
    animations
    some temporary art assets

---
+++ New Unity Project/Assets/Scripts/Renee Test Scripts/LevelLoader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f;
+    public string build;
+    
+    void Update()
+    {   
+        //if (Input.GetKeyDown("0"))
+        //{
+        //    build = 0;
+        //    LoadNextLevel();
+        //}
+    }
+    
+    public void LoadNextLevel(string sceneName)
+    {
+        build = sceneName;
+        StartCoroutine(LoadLevel());
+    }
+
+    IEnumerator LoadLevel()
+    {
+        //play anim
+        transition.SetTrigger("Start");
+        //wait
+        yield return new WaitForSeconds(transitionTime);
+        //load scene
+        SceneManager.LoadScene(build);
+    }
+}
---
+++ New Unity Project/Assets/Scripts/Renee Test Scripts/MouseCursor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCursor : MonoBehaviour
+{
+    public GameObject trailEffect;
+    public GameObject empty;
+    public float size;
+    public float timeSpawn = 0.1f;
+    GameObject obj;
+    void Start()
+    {
+        //Cursor.visible = false;
+    }
+    void Update()
+    {
+        Vector3 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = cursorPos;
+        if (timeSpawn <= 0)
+        {
+
+            obj = Instantiate(trailEffect, cursorPos, Quaternion.identity);
+            timeSpawn = 0.1f;
+
+            obj.transform.parent = empty.transform;
+        }
+        else
+        {
+            //decrease particles when not active
+            timeSpawn -= Time.deltaTime;
+        }
+    }
+}
