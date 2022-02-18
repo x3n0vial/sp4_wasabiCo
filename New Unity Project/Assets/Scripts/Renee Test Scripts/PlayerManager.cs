8737feb2831a33327f9f7a9495cbdd8adbc99458
x3n0vial <69580639+x3n0vial@users.noreply.github.com>
2022-02-18 13:52:11 +0800

    comments for playermanager thing

--- New Unity Project/Assets/Scripts/Renee Test Scripts/PlayerManager.cs
+++ New Unity Project/Assets/Scripts/Renee Test Scripts/PlayerManager.cs
@@ -2,6 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//gets the player for the current scene and sets it
+//to use code in scripts:
+//1) make variable: Transform target;
+//2) call in the start function: target = PlayerManager.instance.player.transform;
+//if still don't understand, can refer to the Minimap.cs script
+//now don't need to keep creating public player objects in the script and keep having to drag and drop the player constantly KSDNFJKSNGSJKN
+
 public class PlayerManager : MonoBehaviour
 {
     #region Singleton
