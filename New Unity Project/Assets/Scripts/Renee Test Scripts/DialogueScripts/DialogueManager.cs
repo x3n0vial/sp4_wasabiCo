4c917f2539357e735a2989b3629a6a75d2a82065
x3n0vial <69580639+x3n0vial@users.noreply.github.com>
2022-02-27 07:46:59 +0800

    added dialogue to washing machine

--- New Unity Project/Assets/Scripts/Renee Test Scripts/DialogueScripts/DialogueManager.cs
+++ New Unity Project/Assets/Scripts/Renee Test Scripts/DialogueScripts/DialogueManager.cs
@@ -16,6 +16,14 @@ public class DialogueManager : MonoBehaviour
         sentences = new Queue<string>();
     }
 
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            DisplayNextSentence();
+        }
+    }
+
     public void StartDialogue(Dialogue dialogue)
     {
         animator.SetBool("IsOpen", true);
