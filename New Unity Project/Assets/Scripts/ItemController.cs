58e4804be20c9861f72166323eb6fa0e89a14bc4
NICOLE CHONG LAY TENG <202435L@mymail.nyp.edu.sg>
2022-02-27 08:25:13 +0800

    changed pickup method

--- New Unity Project/Assets/Scripts/ItemController.cs
+++ New Unity Project/Assets/Scripts/ItemController.cs
@@ -27,16 +27,6 @@ public class ItemController : MonoBehaviour
                 if (hasItem) // drop curr item if player is already holding sth
                 {
 
-                    Collider[] collider1 = Pickable.GetComponents<Collider>();
-                    foreach (Collider d in collider1)
-                    {
-                        d.enabled = true;
-                    }
-                    Pickable.GetComponent<Collider>().enabled = true;
-                    Pickable.GetComponent<Rigidbody>().isKinematic = false; // make the rigidbody work again
-                    Pickable.GetComponent<Rigidbody>().useGravity = true;
-                    Pickable.transform.parent = null;
-                    Debug.Log("Drop");
                 }
 
                 Collider[] collider = Pickable.GetComponents<Collider>();
@@ -48,6 +38,7 @@ public class ItemController : MonoBehaviour
                 Pickable.GetComponent<Rigidbody>().isKinematic = true;   //makes the rigidbody not be acted upon by forces
                 Pickable.transform.position = Slot.transform.position; // sets the position of the object to your hand position
                 Pickable.transform.parent = Slot.transform; //makes the object become a child of the parent so that it moves with the hands
+        
                 Pickable.GetComponent<Rigidbody>().useGravity = false;
                 hasItem = true;
             }
